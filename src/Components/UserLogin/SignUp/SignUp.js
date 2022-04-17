600b8ba1503ea130130a62bb5d89dde9915676c6
Joy Paul <joypaul3592@gmail.com>
2022-04-17 20:06:46 +0600

    adding sign up page

--- src/Components/UserLogin/SignUp/SignUp.js
+++ src/Components/UserLogin/SignUp/SignUp.js
@@ -28,6 +28,10 @@ const Login = () => {
                     <img className='w-[45px] mr-2' src="https://pngimg.com/uploads/google/google_PNG19635.png" alt="" />
                     <h3 className=' font-semibold'>Continue With Google</h3>
                 </div>
+                <div className="flex items-center justify-center w-11/12 md:w-7/12 mx-auto bg-slate-50 rounded-lg shadow-md mb-5" >
+                    <img className='w-[45px] p-1 mr-2' src="https://pngimg.com/uploads/github/github_PNG87.png" alt="" />
+                    <h3 className=' font-semibold'>Continue With GitHub</h3>
+                </div>
             </div>
         </div>
     );
